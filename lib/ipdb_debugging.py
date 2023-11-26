279d5954533fcded75ea639002ac420e6b5289f0
Shaddy <shadybett540@gmail.com>
2023-11-26 21:10:46 +0300

    complete

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,4 +1,10 @@
-import ipdb
-def plus_two(num):
-    return num + 2
-ipdb.set_trace()
+original_list = [2,1,2,1,3,3,4,5,2,3,2]
+
+new_list = list(set(original_list))
+print(new_list)
+
+my_list = ['This is a long sentence', 'word','z']
+
+my_list.sort(key = len , reverse = True)
+print(my_list)
+
